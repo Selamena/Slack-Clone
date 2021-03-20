86d31983fd4ab52b8f054664179805df02b31998
selamawit <selamekale@gmail.com>
2021-03-20 01:01:47 -0400

    SidebarOption

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,22 +2,48 @@ import React from 'react'
 import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
+
 
 const Sidebar = () => {
     return (
-        <SidebarContainer>
-            <SidebarHeader>
-                <SidebarInfo>
-                    <h2>Selamena</h2>
-                    <h3>
-                        <FiberManualRecordIcon />
-                        Selamawit Asmare
-                    </h3>
-                </SidebarInfo>
-                <CreateIcon/>
-             </SidebarHeader>
-       </SidebarContainer>
-    )
+			<SidebarContainer>
+				<SidebarHeader>
+					<SidebarInfo>
+						<h2>Selamena</h2>
+						<h3>
+							<FiberManualRecordIcon />
+							Selamawit Asmare
+						</h3>
+					</SidebarInfo>
+					<CreateIcon />
+				</SidebarHeader>
+
+				<SidebarOption Icon={InsertCommentIcon} title="Threads" />
+				<SidebarOption Icon={InboxIcon} title="Mentions & reaction" />
+				<SidebarOption Icon={DraftsIcon} title="Saved items" />
+				<SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+				<SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+				<SidebarOption Icon={AppsIcon} title="Apps" />
+				<SidebarOption Icon={FileCopyIcon} title="File browser" />
+				<SidebarOption Icon={ExpandLessIcon} title="Show less" />
+				<hr />
+				<SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+				<hr />
+				<SidebarOption Icon={AddIcon} AddChannelOption title="Add Channels" />
+			</SidebarContainer>
+		);
 }
 
 export default Sidebar
@@ -57,4 +83,11 @@ const SidebarInfo = styled.div`
 		font-weight: 400;
         align-items: center;
 	}
+
+    > h3 > .MuiSvgIcon-root{
+        font-size:14px;
+        margin-top:1px;
+        margin-right:2px;
+        color:green;
+    }
 `;
